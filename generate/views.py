d2142e66c1c80f84265da052646920089b082aca
Anastasia Kotsaba <anastasiakocaba49@gmail.com>
2025-02-08 16:43:36 +0200

    new styles

--- generate/views.py
+++ generate/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def render_generate(request):
-    return render(request = request, template_name = 'generate/generate.html')
+    return render(request = request, template_name = 'generate_menu/generate_menu.html')
